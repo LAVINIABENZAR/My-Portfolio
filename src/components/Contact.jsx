e90490f540805a685a92ef0e5138edd49f7bc451
LAVINIABENZAR <loredanabenzar2@gmail.com>
2023-11-24 20:12:14 +0000

    added linkedin svg

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import '../App.css'
+import Linkedin from "../assets/SVGs/Linkedin";
+
 
 
 function Contact() {
@@ -8,7 +10,7 @@ function Contact() {
       <footer className='footer' id='contact'>
     <div className='socials'>
      <p className='email'> <img src="src/assets/email-svgrepo-com.svg" alt="" className="email-svg"/><a href="mailto:loredanabenzar2@gmail.com">EMAIL</a></p>
-     <p className='linkedin'> <img src="src/assets/linkedin-svgrepo-com.svg" alt="" /><a href="https://www.linkedin.com/in/benzar-loredana-80b238201/">LINKEDIN</a></p>
+     <p className='linkedin'><Linkedin/> <a href="https://www.linkedin.com/in/benzar-loredana-80b238201/">LINKEDIN</a></p>
      <p className='github'> <img src="src/assets/github-svgrepo-com.svg" alt="" /><a href="https://github.com/LAVINIABENZAR">GITHUB</a></p>
     </div>
    </footer>
