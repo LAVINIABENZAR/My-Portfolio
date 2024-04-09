e8e2428eb137840071f6112909419c83d68511e4
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-04-09 13:49:15 +0300

    feat: express logo

--- src/App.jsx
+++ src/App.jsx
@@ -93,15 +93,15 @@ function App() {
           <br />
           <div className='skills' id='skills'>
           <div className='tech-stack'><span className='skills-title'>Tech Stack</span>
-                      <p align="center">
+                      <p align="center" className='svgs'>
                        <a href="https://www.prisma.io/" target='_blank' rel='noreffer'> <img src={prisma} alt="Prisma" /></a>
                        <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" /> </a>
-                       <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="70" height="70" /> </a> 
+                       <a href="https://expressjs.com" target="_blank" rel="noreferrer"><span  className='express'>express</span></a> 
                        <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="70" height="70" /> </a> 
                        <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="70" height="70" /> </a> 
                        <a href="https://jasmine.github.io/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/jasmine/jasmine-icon.svg" alt="jasmine" width="70" height="70" /> </a> 
                        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="70" height="70" /> </a> 
-                       <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> <img src={Next} alt="nextjs" width="80" height="70" /></a> 
+                       <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> <img src={Next} alt="nextjs" width="96" height="70" /></a> 
                        <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src={node} alt="nodejs" width="70" height="70" /> </a> 
                        <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="70" height="70" /> </a> 
                        <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="70" height="70" /> </a> </p>
