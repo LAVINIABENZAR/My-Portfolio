d2b166649b885f3a39da658bbc36bc50d74ecd4b
LAVINIABENZAR <loredanabenzar2@gmail.com>
2024-06-25 19:34:33 +0300

    add: tailwind and typescript to tech stack

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Preloader from './components/Preloader.jsx';
 import MyWork from './components/MyWork.jsx';
 import Menu from './components/Menu.jsx';
 import express from './assets/SVGs/express.svg'
+import tailwind from './assets/SVGs/tailwind.svg'
+import typescript from './assets/SVGs/typescript.svg'
 
 
 function App() {
@@ -47,11 +49,17 @@ function App() {
                 <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="70" height="70" /> </a>
                 <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="70" height="70" /> </a>
                 <a href="https://jasmine.github.io/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/jasmine/jasmine-icon.svg" alt="jasmine" width="70" height="70" /> </a>
-                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="70" height="70" /> </a>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="65" height="65" /> </a>
                 <a href="https://nextjs.org/" target="_blank" rel="noreferrer"> <img src={Next} alt="nextjs" width="96" height="70" /></a>
                 <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src={node} alt="nodejs" width="70" height="70" /> </a>
                 <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="70" height="70" /> </a>
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="70" height="70" /> </a> </p>
+                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="70" height="70" /> </a> 
+                <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"> <img src={tailwind} alt="tailwind" width="70" height="70" /> </a>
+                <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer"> <img src={typescript} alt="react" width="65" height="65" /> </a> 
+               
+                </p>
+            
+
             </div>
           </div>
         </div>
